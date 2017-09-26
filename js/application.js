ee3f0ef9968e8e15a152a0ced7eb26effc195e34
Dmitri Stefanisin <email.ds@protonmail.com>
2017-09-26 10:00:14 -0500

    add interface and some refactoring

--- js/application.js
+++ js/application.js
@@ -1,3 +1,32 @@
+var game;
+
 $(document).ready(function() {
-  // $("table td").text(this.array)
+    game = new Game
+    game.updateBoard();
+
+  Mousetrap.bind('enter', function() {
+    game = new Game
+    game.updateBoard();
+  });
+
+  Mousetrap.bind('up', function() {
+    game.moveUp();
+    game.updateBoard();
+  });
+
+  Mousetrap.bind('left', function() {
+    game.moveLeft();
+    game.updateBoard();
+  });
+
+  Mousetrap.bind('right', function() {
+    game.moveRight();
+    game.updateBoard();
+  });
+
+  Mousetrap.bind('down', function() {
+    game.moveDown();
+    game.updateBoard();
+  });
+
 });
--- js/game.js
+++ js/game.js
@@ -17,7 +17,7 @@ Game.prototype.moveUp= function(){
   return this.array
 }
 
-Game.prototype.moveRigth = function(){
+Game.prototype.moveRight = function(){
   this.collapseRight()
   this.addRight()
   this.addNumber()
@@ -30,4 +30,170 @@ Game.prototype.moveLeft = function(){
   this.addNumber()
   return this.array
 }
+
+// *********** getting horizontal & vertical arrays: ***********
+
+Game.prototype.horizontal_arrays = function(){
+  array = this.array.slice();
+  var results = [];
+  while (array.length) {
+    results.push(array.splice(0, 4));
+  }
+  return results;
+}
+
+Game.prototype.vertical_arrays = function(){
+     return _.zip.apply(_, this.horizontal_arrays())
+}
+
+Game.prototype.addNumber = function(){
+  var n = false;
+  do {
+    random = Math.floor(Math.random() * 16) + 1
+    if (this.array[random] === 0){
+      this.array[random] = 2
+      n = true;
+    }
+  }
+  while (!n);
+  return this.array
+}
+
+Game.prototype.updateBoard = function(){
+    for(var i = 0; i < 16; i++){
+    $(`#c${i+1}`).text(game.array[i])
+    if ($(`#c${i+1}`).text() === "0"){
+      $(`#c${i+1}`).text("")   //css("visibility","hidden")
+    }
+  }
+}
+
+// ************* collapsing methods: ********************
+
+Game.prototype.collapseRight = function(){
+  var a = this.horizontal_arrays().map(function(line){
+    for(var i=0; i < line.length; i++){
+        if (line[i] === 0) {
+          line.splice(i,1)
+          line.unshift(0)
+        }
+    }
+    return line
+  })
+  this.array = _.flatten(a);
+  return a
+}
+
+Game.prototype.collapseLeft = function(){
+  var a = this.horizontal_arrays().map(function(line){
+    for(var i=line.length - 1; i > -1; i--){
+        if (line[i] === 0) {
+          line.splice(i,1)
+          line.push(0)
+        }
+    }
+    return line
+  })
+  this.array = _.flatten(a);
+  return a
+}
+
+Game.prototype.collapseUp = function(){
+  var a = this.vertical_arrays().map(function(line){
+    for(var i=line.length - 1; i > -1; i--){
+        if (line[i] === 0) {
+          line.splice(i,1)
+          line.push(0)
+        }
+    }
+    return line
+  })
+  a = _.zip.apply(_, a)
+  this.array = _.flatten(a);
+  return a
+}
+
+Game.prototype.collapseDown = function(){
+  var a = this.vertical_arrays().map(function(line){
+    for(var i=0; i < line.length; i++){
+        if (line[i] === 0) {
+          line.splice(i,1)
+          line.unshift(0)
+        }
+    }
+    return line
+  })
+  a = _.zip.apply(_, a)
+  this.array = _.flatten(a);
+  return a
+}
+
+//************ adding numbers in rows:*****************
+
+Game.prototype.addRight = function(){
+  var a = this.horizontal_arrays().map(function(line){
+    for(var i=line.length - 1; i > -1; i--){
+        if (line[i] === line[i - 1]) {
+          sum = line[i] + line[i - 1]
+          line.splice(i, 1, sum)
+          line.splice(i - 1, 1)
+          line.unshift(0)
+        }
+    }
+    return line
+  })
+  this.array = _.flatten(a);
+  return a
+}
+
+Game.prototype.addLeft = function(){
+  var a = this.horizontal_arrays().map(function(line){
+    for(var i=0; i < line.length; i++){
+        if (line[i] === line[i + 1]) {
+          sum = line[i] + line[i + 1]
+          line.splice(i, 1, sum)
+          line.splice(i + 1, 1)
+          line.push(0)
+        }
+    }
+    return line
+  })
+  this.array = _.flatten(a);
+  return a
+}
+
+Game.prototype.addUp= function(){
+  var a = this.vertical_arrays().map(function(line){
+    for(var i=0; i < line.length; i++){
+        if (line[i] === line[i + 1]) {
+          sum = line[i] + line[i + 1]
+          line.splice(i, 1, sum)
+          line.splice(i + 1, 1)
+          line.push(0)
+        }
+    }
+    return line
+  })
+  a = _.zip.apply(_, a)
+  this.array = _.flatten(a);
+  return a
+}
+
+Game.prototype.addDown = function(){
+  var a = this.vertical_arrays().map(function(line){
+    for(var i=line.length - 1; i > -1; i--){
+        if (line[i] === line[i - 1]) {
+          sum = line[i] + line[i - 1]
+          line.splice(i, 1, sum)
+          line.splice(i - 1, 1)
+          line.unshift(0)
+        }
+    }
+    return line
+  })
+  a = _.zip.apply(_, a)
+  this.array = _.flatten(a);
+  return a
+}
+
 
